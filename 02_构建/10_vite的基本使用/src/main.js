9bef245f99227a901259bc0a1f3aad1c85a30915
zhangzhibang0309 <295854713@qq.com>
2022-02-07 02:52:26 +0800

    构建 over

--- 02_构建/10_vite的基本使用/src/main.js
+++ 02_构建/10_vite的基本使用/src/main.js
@@ -1,11 +1,15 @@
 import _ from "lodash";
+import { createApp } from "vue";
 import { sum } from "./js/math";
+import mul from "./ts/mul";
+import App from "./vue/App.vue";
 
 import "./css/style.css";
 import "./css/title.less";
 
 console.log("Hello World");
 console.log(sum(1, 2));
+console.log(mul(20, 30));
 
 console.log(_.join(["abc", "cba"], "-"));
 console.log(111);
@@ -14,3 +18,6 @@ const titleEl = document.createElement("div");
 titleEl.className = "title";
 titleEl.innerHTML = "Hello vite";
 document.body.append(titleEl);
+
+// vue
+createApp(App).mount("#app");
---
+++ 02_构建/10_vite的基本使用/src/vue/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>{{ message }}</h2>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Hello Vue"
+        }
+    }
+}
+</script>
+
+<style>
+h2 {
+    color: white;
+}
+</style>
